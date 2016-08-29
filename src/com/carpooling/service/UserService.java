d4a5425afdc0653245e66368ba6a139c656a45dc
gkdangal <gkdangal@gmail.com>
2016-08-29 01:07:06 -0600

    Service package created

---
+++ src/com/carpooling/service/UserService.java
@@ -0,0 +1,16 @@
+package com.carpooling.service;
+
+import com.carpooling.dao.UserDAO;
+
+public class UserService {
+	UserDAO userDAO = new UserDAO();
+
+	public boolean isUser(String email, String password) {
+		return userDAO.isUser(email, password);
+	}
+
+	public boolean createUser(String fullname, int gender, String state, String city, String street, int zip,
+			int birthyear, String email, String password) {
+		return userDAO.createUser(fullname, gender, state, city, street, zip, birthyear, email, password);
+	}
+}
